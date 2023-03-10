318859b47215dd5944d956c8139567e2271cdc7d
Erik <abtahe19@gmail.com>
2023-03-11 01:50:01 +0600

    main branch changed

--- app/src/main/java/com/example/jobportalapp/Extra.java
+++ app/src/main/java/com/example/jobportalapp/Extra.java
@@ -3,8 +3,8 @@ package com.example.jobportalapp;
 public class Extra {
     public static void main(String[] args){
         System.out.println("Hello, world");
-        System.out.println("BUET");
-        System.out.println("EEE");
+        System.out.println("KUET");
+        System.out.println("CSE");
 
     }
 }

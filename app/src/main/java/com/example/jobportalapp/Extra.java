3920141dc1bfb3aeb1d6b019fe94f8be6c4f8e7f
Erik <abtahe19@gmail.com>
2023-03-11 02:21:08 +0600

    developer2 changed

--- app/src/main/java/com/example/jobportalapp/Extra.java
+++ app/src/main/java/com/example/jobportalapp/Extra.java
@@ -3,8 +3,8 @@ package com.example.jobportalapp;
 public class Extra {
     public static void main(String[] args){
         System.out.println("Hello, world");
-        System.out.println("OXFORD");
-        System.out.println("IEM");
+        System.out.println("KUET");
+        System.out.println("CSE");
 
     }
 }

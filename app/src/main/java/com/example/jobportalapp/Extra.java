dec80e697c09aee31a021dbc2b4d8a7494fb96f5
Erik <abtahe19@gmail.com>
2023-03-11 01:39:50 +0600

    developer2 branch changed

--- app/src/main/java/com/example/jobportalapp/Extra.java
+++ app/src/main/java/com/example/jobportalapp/Extra.java
@@ -3,8 +3,8 @@ package com.example.jobportalapp;
 public class Extra {
     public static void main(String[] args){
         System.out.println("Hello, world");
-        System.out.println("KUET");
-        System.out.println("CSE");
+        System.out.println("BUET");
+        System.out.println("EEE");
 
     }
 }

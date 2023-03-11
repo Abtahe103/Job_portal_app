601acb0d212105a683742df0e692c5653171800c
Demonstar103 <74531694+Demonstar103@users.noreply.github.com>
2023-03-11 23:59:06 +0600

    Update Extra.java

--- app/src/main/java/com/example/jobportalapp/Extra.java
+++ app/src/main/java/com/example/jobportalapp/Extra.java
@@ -1,6 +1,6 @@
 package com.example.jobportalapp;
 public class Extra{
-  public static void main{
+  public static void main(String[] args){
     System.out.println("Hello world!");
     System.out.println("KUET");
     System.out.println("CSE");
